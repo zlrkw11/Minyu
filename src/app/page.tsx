b40b1b55b1ec70b762bbe5aeeff567a9e6ec38ad
zlrkw11 <ray040406@gmail.com>
2024-10-19 15:04:17 +1300

    home page structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className="items-center justify-center flex flex-col m-8">
+      <h1 className="text-3xl">Profile picture</h1>
+      <p>hello</p>
+      <div className="border border-black py-8 w-full"></div>
+    </div>
+  );
 }
