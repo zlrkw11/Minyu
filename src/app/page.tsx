2675522b7626328d7de232987084551a39582848
zlrkw11 <ray040406@gmail.com>
2024-10-19 15:57:01 +1300

    card props

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 import Image from "next/image";
 import Heart from "@/app/assets/heart.png";
 import Like from "@/app/assets/like.svg";
-
+export interface CardProps {
+  text: string;
+}
 export default function Home() {
   return (
     <div className="items-center justify-center flex flex-col m-8 gap-8">
@@ -17,13 +19,14 @@ export default function Home() {
     </div>
   );
 }
-
-const Card = () => {
+type props = CardProps;
+const Card = ({ text }: props) => {
   "use client";
   return (
-    <div className="rounded-md border-4 flex justify-center items-center border-slate-700 hover:bg-slate-400 hover:border-slate-200 duration-500 py-16 w-full bg-slate-600">
-      <div className="text-slate-200">
-        <p>Hello this is a testing text</p>
+    <div className="rounded-md border-4 gap-4 flex justify-center items-center border-slate-700 hover:bg-slate-400 hover:border-slate-200 duration-500 py-16 w-full bg-slate-600">
+      <div className="text-slate-200 m-4">
+        <p>Hello this is a testing text ghoongongojgfenaefafafawfafawf</p>
+        {text}
       </div>
       <Image
         className="cursor-pointer ml-auto mr-8 fill-white"
