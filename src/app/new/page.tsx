5ad3e6e82a21873d692dcafa1e1575f6e8a7275a
zlrkw11 <ray040406@gmail.com>
2024-10-19 16:29:37 +1300

    input box

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,8 +1,8 @@
 export default function New() {
   return (
-    <div className="flex flex-col items-center justify-center p-4">
+    <div className="flex flex-col items-center justify-center p-4 gap-8">
       <h1 className="text-3xl font-bold">Make a new blog</h1>
-      <input className="border border-black rounded-md"></input>
+      <input className="border border-black rounded-md w-full h-16 justify-center p-4 flex-col flex"></input>
     </div>
   );
 }
