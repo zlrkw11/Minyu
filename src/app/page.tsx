7a423cf6c52d196755389e8224b53b76048ef95c
zlrkw11 <ray040406@gmail.com>
2024-10-19 15:27:08 +1300

    like svg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,26 @@
+import Image from "next/image";
+import Like from "@/app/assets/like.svg";
 export default function Home() {
   return (
-    <div className="items-center justify-center flex flex-col m-8">
+    <div className="items-center justify-center flex flex-col m-8 gap-8">
       <h1 className="text-3xl">Profile picture</h1>
       <p>hello</p>
-      <div className="border border-black py-8 w-full"></div>
+
+      <div className="flex flex-col justify-center items-center w-full gap-4">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
     </div>
   );
 }
+
+const Card = () => {
+  "use client";
+  return (
+    <div className="rounded-md border-4 border-slate-700 hover:bg-slate-400 hover:border-slate-200 duration-500 py-16 w-full bg-slate-600">
+      <Image className="ml-auto mr-8" src={Like} width={50} alt={""} />
+    </div>
+  );
+};
