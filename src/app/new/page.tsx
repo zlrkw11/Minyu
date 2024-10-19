83b77d016255b4d33a65e926ca991463255b052c
zlrkw11 <ray040406@gmail.com>
2024-10-19 16:27:26 +1300

    new blog page

---
+++ src/app/new/page.tsx
@@ -0,0 +1,8 @@
+export default function New() {
+  return (
+    <div className="flex flex-col items-center justify-center p-4">
+      <h1 className="text-3xl font-bold">Make a new blog</h1>
+      <input className="border border-black rounded-md"></input>
+    </div>
+  );
+}
