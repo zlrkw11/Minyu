e45d16e42a0ea255b5238ebaf4821600018be133
zlrkw11 <ray040406@gmail.com>
2024-10-19 18:05:18 +1300

    mapping frontend

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,29 +2,26 @@ import Image from "next/image";
 import Like from "@/app/assets/like.svg";
 import { getBlogs } from "@/routes/blogRoutes";
 
-const blogs = getBlogs();
-
 export interface CardProps {
   text?: string;
 }
-export default function Home() {
+export default async function Home() {
+  const blogs = await getBlogs();
   return (
     <div className="items-center justify-center flex flex-col m-8 gap-8">
       <h1 className="text-3xl">Profile picture</h1>
       <p>some random quotes</p>
 
       <div className="flex flex-col justify-center items-center w-full gap-4">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        {blogs.map((blog, index) => (
+          <Card key={index} text={blog.content} />
+        ))}
       </div>
     </div>
   );
 }
 type props = CardProps;
 const Card = ({ text }: props) => {
-  "use client";
   return (
     <div className="rounded-md border-4 gap-4 flex justify-center items-center border-slate-700 hover:bg-slate-400 hover:border-slate-200 duration-500 py-16 w-full bg-slate-600">
       <div className="text-slate-200 m-4">{text}</div>
